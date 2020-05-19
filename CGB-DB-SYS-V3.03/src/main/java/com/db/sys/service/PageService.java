ccee108507c60f6451e8933140b0dc7f69d72790
yaojie-oos <65456391+yaojie-oos@users.noreply.github.com>
2020-05-19 11:23:22 +0800

    Update PageService.java

--- CGB-DB-SYS-V3.03/src/main/java/com/db/sys/service/PageService.java
+++ CGB-DB-SYS-V3.03/src/main/java/com/db/sys/service/PageService.java
@@ -1,7 +1,7 @@
 package com.db.sys.service;
 
 import com.db.common.vo.PageObject;
-
+//111111111111111
 public interface PageService<T> {
 	PageObject<T> findPageObjects(Integer pageCurrent,
 			String username);
