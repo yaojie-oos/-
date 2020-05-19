f80a3070a299e36066d2a993ca085bd549bc677e
yaojie <1171646049@qq.com>
2020-05-19 11:24:49 +0800

    添加注释

--- CGB-DB-SYS-V3.03/src/main/java/com/db/sys/service/PageService.java
+++ CGB-DB-SYS-V3.03/src/main/java/com/db/sys/service/PageService.java
@@ -1,7 +1,7 @@
 package com.db.sys.service;
 
 import com.db.common.vo.PageObject;
-
+//123123123
 public interface PageService<T> {
 	PageObject<T> findPageObjects(Integer pageCurrent,
 			String username);
